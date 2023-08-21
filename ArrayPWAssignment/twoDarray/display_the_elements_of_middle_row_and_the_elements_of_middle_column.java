7d4be0bd7fe70f2e6271c2d1c45e1c713d547a4e
SHIVSHANKAR KUMAR <122685683+SHIVSHANKARKUMAR62@users.noreply.github.com>
2023-08-21 23:50:15 +0530

    Update display_the_elements_of_middle_row_and_the_elements_of_middle_column.java

--- ArrayPWAssignment/twoDarray/display_the_elements_of_middle_row_and_the_elements_of_middle_column.java
+++ ArrayPWAssignment/twoDarray/display_the_elements_of_middle_row_and_the_elements_of_middle_column.java
@@ -2,6 +2,20 @@ package Data_Structure.ArrayPWAssignment.twoDarray;
 
 import java.util.Scanner;
 
+class Calculation{
+    public void cal(int [][]arr,int m,int n){
+        System.out.print("The elements of both the diagonals are: ");
+
+        for (int i=0;i<n;i++) {
+            for (int j = 0; j < m; j++) {
+                if (i == (n-1)/2 || j == (n-1)/2) {
+                    System.out.print(arr[i][j] + " ");
+                }
+            }
+        }
+    }
+}
+
 public class display_the_elements_of_middle_row_and_the_elements_of_middle_column {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
@@ -24,15 +38,8 @@ public class display_the_elements_of_middle_row_and_the_elements_of_middle_colum
             System.out.println();
         }
 
+        Calculation calculation = new Calculation(); // creating the object of the class.
+        calculation.cal(arr,n,m); // call the function or method 
 
-        System.out.print("The elements of both the diagonals are: ");
-
-        for (int i=0;i<n;i++) {
-            for (int j = 0; j < m; j++) {
-                if (i == (n-1)/2 || j == (n-1)/2) {
-                    System.out.print(arr[i][j] + " ");
-                }
-            }
-        }
     }
 }
